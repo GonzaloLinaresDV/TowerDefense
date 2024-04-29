2b07fd36222b2c1bda1b9aba785ed001263e089b
GonzaloLinaresDV <gonzalo.daniel.linares@gmail.com>
2024-04-28 22:42:47 -0300

    Add - Enemy List in GameManager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     public List<Enemy> enemyList;
     public Transform start, end;
     public List<FireTower> fireTowers;
+    public List<Enemy> allEnemy;
     // Start is called before the first frame update
     void Start()
     {
